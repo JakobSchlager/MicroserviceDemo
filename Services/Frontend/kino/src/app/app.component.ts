a9ddf724b9b26f9dfac786800e4d6c72bf1e0059
Jakob Schlager <jakob.schlager.biz@gmail.com>
2021-10-23 16:35:11 +0200

    add frontend folder and progressbar

--- Services/Frontend/kino/src/app/app.component.ts
+++ Services/Frontend/kino/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IMovie } from './interfaces';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  readonly address: string = `http://localhost`; 
   title = 'kino';
+ 
+  status: number = 100; 
+  setStatus(percent: number) {
+    this.status = percent; 
+  }
+
+  movies: IMovie[] = []; 
+  
+  ngOnInit(): void {
+    
+  }
+  
 }
